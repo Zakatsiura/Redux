3f995e4dc328a5685c9aaeab5c24f9a343092908
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-05-09 11:43:13 +0300

    todoRedux

--- src/components/TodoRedux/store.js
+++ src/components/TodoRedux/store.js
@@ -27,7 +27,7 @@ const todos = (state = initialState, action) => {
     }
 };
 
-export const store = createStore(todos);
+export const store = createStore(todos, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 // action creators
 export const addTodo = (title) => ({
