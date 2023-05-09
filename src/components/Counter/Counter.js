f76a4d90206ba58f95e7eb237f39b13fb6450bd0
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-05-09 09:51:19 +0300

    counter/counters

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,50 +1,48 @@
-import React from 'react';
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement, reset } from "./store";
-
-
-const Counter = () => {
-
-    const count = useSelector((state) => state);
-    const dispatch = useDispatch();
-
-    return (
-        <div>
-            <h2>{count}</h2>
-            <button onClick={() => dispatch(decrement)}>-</button>
-            <button onClick={() => dispatch(increment)}>+</button>
-            <button onClick={() => dispatch(reset)}>reset</button>
-        </div>
-    );
-
-};
-export default Counter;
-
-// FOR TWO SEPARATED COUNTERS:
-//
 // import React from 'react';
 // import { useSelector, useDispatch } from "react-redux";
-// import { increment1, decrement1, reset1, increment2, decrement2, reset2 } from "./store";
+// import { increment, decrement, reset } from "./store";
 //
 //
 // const Counter = () => {
 //
-//     const count1 = useSelector((state) => state.count1);
-//     const count2 = useSelector((state) => state.count2);
+//     const count = useSelector((state) => state);
 //     const dispatch = useDispatch();
 //
 //     return (
 //         <div>
-//             <h2>Counter 1: {count1}</h2>
-//             <button onClick={() => dispatch(decrement1())}>-</button>
-//             <button onClick={() => dispatch(increment1())}>+</button>
-//             <button onClick={() => dispatch(reset1())}>reset</button>
-//             <h2>Counter 2: {count2}</h2>
-//             <button onClick={() => dispatch(decrement2())}>-</button>
-//             <button onClick={() => dispatch(increment2())}>+</button>
-//             <button onClick={() => dispatch(reset2())}>reset</button>
+//             <h2>{count}</h2>
+//             <button onClick={() => dispatch(decrement)}>-</button>
+//             <button onClick={() => dispatch(increment)}>+</button>
+//             <button onClick={() => dispatch(reset)}>reset</button>
 //         </div>
 //     );
 //
 // };
 // export default Counter;
+
+// FOR TWO SEPARATED COUNTERS:
+
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment1, decrement1, reset1, increment2, decrement2, reset2 } from "./store";
+
+const Counter = () => {
+    const count1 = useSelector((state) => state.count1);
+    const count2 = useSelector((state) => state.count2);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <h2>First Counter is equal to {count1}</h2>
+            <button onClick={() => dispatch(decrement1())}>-</button>
+            <button onClick={() => dispatch(increment1())}>+</button>
+            <button onClick={() => dispatch(reset1())}>reset</button>
+            <h2>Second Counter is equal to {count2}</h2>
+            <button onClick={() => dispatch(decrement2())}>-</button>
+            <button onClick={() => dispatch(increment2())}>+</button>
+            <button onClick={() => dispatch(reset2())}>reset</button>
+        </div>
+    );
+};
+
+export default Counter;
